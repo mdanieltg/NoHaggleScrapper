cd24d10d6d42d87ccf3c202c7d02a2432f0e2ebf
Daniel Medrano Huerta <daniel@quicktortoise.com>
2024-08-24 15:25:10 -0600

    Performance improvements to the WebClient

--- NoHaggleScrapper.HttpApi/Services/WebClient.cs
+++ NoHaggleScrapper.HttpApi/Services/WebClient.cs
@@ -1,11 +1,28 @@
-﻿using NoHaggleScrapper.HttpApi.Models;
+﻿using System.Net;
+using System.Net.Http.Headers;
+using System.Security.Authentication;
+using NoHaggleScrapper.HttpApi.Models;
 
 namespace NoHaggleScrapper.HttpApi.Services;
 
 public class WebClient
 {
-    private readonly ILogger<WebClient> _logger;
+    private static readonly HttpClientHandler HttpClientHandler = new()
+    {
+        AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate,
+        SslProtocols = SslProtocols.Tls12 | SslProtocols.Tls13
+    };
+
+    private static readonly MediaTypeWithQualityHeaderValue AcceptEverything = new("*/*");
+    private static readonly StringWithQualityHeaderValue AcceptEncodingGzip = new("gzip");
+    private static readonly StringWithQualityHeaderValue AcceptEncodingDeflate = new("deflate");
+    private static readonly StringWithQualityHeaderValue AcceptLangEn = new("en-US", 0.9);
+    private static readonly StringWithQualityHeaderValue AcceptLangEnUs = new("en", 0.8);
+
     private readonly HttpClient _httpClient;
+    private readonly ILogger<WebClient> _logger;
+    private short _forbiddenCount;
+    private bool _isBlocked;
 
     private WebClient(ILogger<WebClient> logger, HttpClient httpClient)
     {
@@ -23,6 +40,10 @@ public class WebClient
         //   in the constructor
         Uri callingUri = uri ?? BaseUrl;
         WebResult result = new() { Uri = callingUri, BaseUrl = BaseUrl };
+
+        // If we are blocked, then do nothing else
+        if (_isBlocked) return result;
+
         try
         {
             HttpResponseMessage response = await _httpClient.GetAsync(uri, cancellationToken);
@@ -33,10 +54,21 @@ public class WebClient
             result.Html = await response.Content.ReadAsStringAsync(cancellationToken);
             _logger.LogDebug("Successful response from URL {Url}", callingUri);
         }
-        catch (HttpRequestException e)
+        catch (HttpRequestException e) when (e.StatusCode is not null)
         {
             _logger.LogError("An HTTP {StatusCode} error occurred while processing the URL {Url} with description: {ErrorDescription}",
                 e.StatusCode, callingUri, e.Message);
+
+            if (e.StatusCode == HttpStatusCode.Forbidden && ++_forbiddenCount == 10)
+            {
+                _isBlocked = true;
+                _logger.LogWarning("The host {Host} is now blocking this WebClient requests", BaseUrl.Host);
+            }
+        }
+        catch (HttpRequestException e)
+        {
+            _logger.LogError("An error occurred while processing the URL {Url} with description: {ErrorDescription}",
+                callingUri, e.Message);
         }
 
         return result;
@@ -44,14 +76,25 @@ public class WebClient
 
     public static WebClient CreateHttpClient(Uri baseAddress, ILogger<WebClient> logger)
     {
-        HttpClient httpClient = new()
+        HttpClient httpClient = new(HttpClientHandler)
         {
-            BaseAddress = baseAddress
+            BaseAddress = baseAddress,
+            DefaultRequestHeaders =
+            {
+                Accept = { AcceptEverything },
+                AcceptEncoding = { AcceptEncodingGzip, AcceptEncodingDeflate },
+                AcceptLanguage = { AcceptLangEnUs, AcceptLangEn },
+                Connection = { "keep-alive" },
+                UserAgent =
+                {
+                    // Firefox user agent
+                    ProductInfoHeaderValue.Parse("Mozilla/5.0"),
+                    ProductInfoHeaderValue.Parse("(Windows NT 10.0; Win64; x64; rv:129.0)"),
+                    ProductInfoHeaderValue.Parse("Gecko/20100101"),
+                    ProductInfoHeaderValue.Parse("Firefox/129.0")
+                }
+            }
         };
-        httpClient.DefaultRequestHeaders.Clear();
-        httpClient.DefaultRequestHeaders.Add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7");
-        httpClient.DefaultRequestHeaders.Add("Accept-Language", "en");
-        httpClient.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36");
 
         return new WebClient(logger, httpClient);
     }
